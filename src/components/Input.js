87df4aa3864339b940322e18b6aadc6c6872f8bf
oudzz <awadjoseph964@gmail.com>
2021-10-26 22:07:27 +0300

    calculator

---
+++ src/components/Input.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Input(){
+    return(
+        <div class="container">
+        <form>
+          <input type="text" value={result}></input>
+        </form>
+      </div>
+    )
+}
+
+
+export default Input;
---
+++ src/components/Keypad.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Keypad() {
+    return (
+        <div class="keypad">
+            <button>clear</button>
+            <button>C</button>
+            <button>&divide;</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>&times;</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>&ndash;</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+        </div>
+    )
+}
+export default Keypad;
